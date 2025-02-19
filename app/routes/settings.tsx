5c25db2ccf18864e09f844142d191d1985a24d1b
Danny Redden <dredden@allegion.com>
2025-02-19 17:18:01 -0500

    added loader function for practice

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,22 @@
 import { Link, Outlet } from "react-router";
+import type { Route } from "./+types/settings";
 
-export default function Settings() {
+export async function loader({params} : Route.LoaderArgs) {
+  return new Response(JSON.stringify({message: "Hello there!"}), {
+    status: 418,
+    headers: {
+      "Content-Type": "application/json",
+      custom: "custom header",
+    },
+  });
+};
+
+export default function Settings({loaderData}: Route.ComponentProps) {
   return (
     <div>
       <h2>Settings</h2>
       <p>This is the settings page</p>
+      <p>This is a message from loader: {loaderData}</p>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
